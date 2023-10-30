0f1f8ad4d63af7f3a4d7e7071bec6eff6c9761f3
yiyu <yiyu4755@gmail.com>
2023-10-30 17:44:37 +0800

    基于阻塞队列实现秒杀异步下单（无法加载）

--- src/main/java/com/ulife/utils/SimpleRedisLock.java
+++ src/main/java/com/ulife/utils/SimpleRedisLock.java
@@ -1,13 +1,9 @@
 package com.ulife.utils;
 
 import cn.hutool.core.lang.UUID;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.data.redis.core.script.RedisScript;
 
 
-import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -20,14 +16,6 @@ public class SimpleRedisLock implements ILock {
     public static final String KEY_PREFIX = "lock:";
     //用户标识前缀
     public static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
-    //静态初始化lua脚本
-    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
-
-    static {
-        UNLOCK_SCRIPT = new DefaultRedisScript<>();
-        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unLock.lua"));
-        UNLOCK_SCRIPT.setResultType(Long.class);
-    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -51,32 +39,19 @@ public class SimpleRedisLock implements ILock {
     }
 
     /**
-     * lua脚本
+     * 释放锁
      */
     @Override
     public void unLock() {
-        stringRedisTemplate.execute(
-                UNLOCK_SCRIPT,
-                Collections.singletonList(KEY_PREFIX + name),
-                ID_PREFIX + Thread.currentThread().getId());
+        //1.判断是否是当前用户
+        //1.1 获取当前线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        //1.2 获取redis中线程标识
+        String redisId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        //2.判断标识是否一致
+        if (redisId.equals(threadId)) {
+            //一致，则是相同线程，释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
-
-
-    /**
-     * 释放锁
-     */
-    // @Override
-    // public void unLock() {
-    //     //1.判断是否是当前用户
-    //     //1.1 获取当前线程标识
-    //     String threadId = ID_PREFIX + Thread.currentThread().getId();
-    //     //1.2 获取redis中线程标识
-    //     String redisId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-    //     //2.判断标识是否一致
-    //     if (redisId.equals(threadId)) {
-    //         //一致，则是相同线程，释放锁
-    //         stringRedisTemplate.delete(KEY_PREFIX + name);
-    //     }
-    // }
-
 }
